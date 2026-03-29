91f8aa8af33985bf8bf1890ffd2f7f9a0ec2bb2e
Vedant Kulkarni <vedantkulkarni9@gmail.com>
2026-03-29 22:04:47 +0530

    add all changes

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -1,11 +1,14 @@
 package cucumber;
 
+import org.testng.annotations.Listeners;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 //cucumber->  TestNG, junit
 
 @CucumberOptions(features="src/test/java/cucumber",glue="rahulshettyacademy.stepDefinitions",
 monochrome=true, tags = "@Regression", plugin= {"html:target/cucumber.html"})
+@Listeners(rahulshettyacademy.TestComponents.Listeners.class)
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 
 	
